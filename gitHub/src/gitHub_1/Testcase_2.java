8948c558a41c48c05486baac0f7a810ad71bff50
user1 <user1@TANUJ>
2022-03-01 23:37:51 +0530

    3rd commint

--- gitHub/src/gitHub_1/Testcase_2.java
+++ gitHub/src/gitHub_1/Testcase_2.java
@@ -1,5 +1,10 @@
 package gitHub_1;
 
-public class Testcase_2 {
+public class Testcase_2 
+{
+	public static void main(String[] args)
+	{
+	System.out.println( "testign 2nd phase");
+	}
 
 }
--- gitHub/src/gitHub_1/Testing_1.java
+++ gitHub/src/gitHub_1/Testing_1.java
@@ -5,6 +5,10 @@ public class Testing_1 {
 	public static void main(String[] args)
 	{
 	System.out.println( "hello world");
+	System.out.println( "hello world-2");
+	System.out.println( "hello world");
+	System.out.println( "herld");
+	
 	}
 
 }
